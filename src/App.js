05529bc2fbd7d8aea8447ae99e61afba42fa4398
Maya Safadi <safadimaya@gmail.com>
2018-10-17 15:32:51 +0200

    appli finale simpsons quote random

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Quote from './Components/Quote'
+import GenerateQuote from './Components/GenerateQuote'
+
+const quoteSample =
+    {
+    "quote": "Shoplifting is a victimless crime, like punching someone in the dark.",
+    "character": "Nelson Muntz",
+    "image" : "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+    "characterDirection" : "Left"
+    }
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            newQuote : quoteSample
+        };
+    }
+
+    selectQuote = () => {
+        fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
+        .then(response  =>  response.json())
+        .then(data  => {
+            this.setState({
+                newQuote :  data[0],
+            });
+        });
+    };
+
+    render() {
+
+        return (
+            <div className="App">
+                <GenerateQuote selectQuote = {this.selectQuote}/>
+                <Quote quote = {this.state.newQuote.quote} character = {this.state.newQuote.character} image = {this.state.newQuote.image} />
+            </div>
+        );
+    }
 }
 
 export default App;
